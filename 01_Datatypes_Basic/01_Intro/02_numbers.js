31ac6a6d1098158000677ce45b39ac8df37b2fa0
Daniel Garavaldi <daniel.garavaldi@bzz.ch>
2023-08-31 10:51:09 +0200

    More examples 01_*

--- 01_Datatypes_Basic/01_Intro/02_numbers.js
+++ 01_Datatypes_Basic/01_Intro/02_numbers.js
@@ -6,8 +6,7 @@ Thema: Variablen, Zahlen
 Beispiel - Umwandlung eines Menschenalters in Hundejahre
  */
 
-let age = 26;
-let dogYears = (age + 1) / 7;
+let humanYears = 26;
+let dogYears = (humanYears + 1) / 7;
 
-console.log(dogYears);
 console.log(`Hundejahre sind: ${dogYears}`);
