559de99165d3d364a90af5e2397fc35b1a0c44a7
Daniel Garavaldi <daniel.garavaldi@bzz.ch>
2023-09-14 10:02:34 +0200

    02_Intro

--- 02_FlowControl_Basic/01_Intro/01_if-then-else.js
+++ 02_FlowControl_Basic/01_Intro/01_if-then-else.js
@@ -3,7 +3,7 @@ Thema: if-then-else ("wenn-dann-sonst", conditional statements)
     und switch-case
  */
 /*
-Beispiel 1a - Traffic Light
+Example 1 - Traffic Light
  */
 
 //Input
@@ -24,71 +24,22 @@ else {
 }
 
 /*
-Beispiel 1b - Traffic Light with switch case
- */
-
-//Input
-color = 'red';
-console.log(`Traffic light is ${color}`);
-//Verarbeitung
-switch (color){
-    case 'red':
-        console.log(`DON'T WALK`);
-        break;
-    case 'orange':
-        console.log(`ATTENTION`);
-        break;
-    case 'green':
-        console.log(`WALK`);
-        break;
-    default:
-        console.log(`OUT OF ORDER`);
-}
-
-
-/*
-Beispiel 2 - Ist der Benutzeraccount gesperrt?
- */
-
-//Vorbedingungen
-let isAccountLocked = false;
-let userRole = 'user';
-
-//unabhängige Überprüfungen und Steuerung
-if (isAccountLocked) {
-    console.log('========\nYour account is locked! Wait 5 minutes.\n========\n');
-}
-if (userRole === 'admin') {
-    console.log('Welcome Admin');
-}
-if (userRole === 'user') {
-    console.log('Welcome User');
-}
-
-//abhängige Überprüfungen und deshalb effizienter
-if (isAccountLocked) {
-    console.log('========\nYour account is locked! Wait 5 minutes.\n========\n');
-} else if (userRole === 'admin') {
-    console.log('Welcome Admin');
-} else {
-    console.log('Welcome User!');
-}
-
-
-/*
-Beispiel 3 - Ist es kalt oder warm?
+Example 2 - Is it cold or warm?
  */
 
 //Input
 let temp = 4;
 
 //Überprüfung + Steuerung
-if (temp <= 4) {
-    console.log('It is freezing outside');
-} else if (temp >= 40) {
-    console.log('It is hot outside');
+let temp = 23;
+if (temp < 4){
+    console.log(`It's ${temp}°C and it's freezing. Please take your coat!`)
+} else if (4 <= temp && temp <=10){
+    console.log(`It's ${temp}°C and it's cold. Please take your pullover!`)
+} else if (11 <= temp && temp <=20){
+    console.log(`It's ${temp}°C and it's pleasant. Please take only a light jacket!`)
 } else {
-    console.log('Go for it. It is pretty nice out');
+    console.log(`It's ${temp}°C and no advise available!`)
 }
 
 
